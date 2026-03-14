30804faae3aac64a1a7006a883c0d25907c638af
Athul Anil Thomas <athulanilthomas@gmail.com>
2026-03-14 16:57:01 +0530

    feat(api): add service layer

--- apps/api/go.mod
+++ apps/api/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/panjf2000/ants/v2 v2.11.6
 	github.com/zmb3/spotify/v2 v2.4.3
+	go.uber.org/fx v1.24.0
 	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5
 	golang.org/x/sync v0.20.0
 )
@@ -13,7 +14,11 @@ require (
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
+	go.uber.org/dig v1.19.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
 )
--- apps/api/internal/service/spotify.go
+++ apps/api/internal/service/spotify.go
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/sync/singleflight"
 )
 
-type Cache struct {
+type cache struct {
 	result    *spotify.CurrentlyPlaying
 	fetchedAt time.Time
 	mu        sync.RWMutex
@@ -18,20 +18,75 @@ type Cache struct {
 
 type Service struct {
 	client *spotify.Client
-	pool   *ants.Pool
+	pool   *ants.PoolWithFunc
 	group  singleflight.Group
-	cache  Cache
+	cache  cache
 }
 
-type Job struct {
+type job struct {
 	ctx    context.Context
 	result chan *spotify.CurrentlyPlaying
 	err    chan error
 }
 
-func NewService(client *spotify.Client, pool *ants.Pool) *Service {
-	// TODO: Implementation
-	return &Service{
-		client: client,
+func NewService(client *spotify.Client) (*Service, error) {
+	pool, err := ants.NewPoolWithFunc(10, func(payload any) {
+		job := payload.(*job)
+		if c, err := client.PlayerCurrentlyPlaying(job.ctx); err != nil {
+			job.err <- err
+		} else {
+			job.result <- c
+		}
+	})
+
+	if err != nil {
+		return nil, err
 	}
+
+	return &Service{
+		pool:   pool,
+		client: client,
+		group:  singleflight.Group{},
+		cache:  cache{},
+	}, nil
+}
+
+func (s *Service) GetCurrentlyPlaying(ctx context.Context) (*spotify.CurrentlyPlaying, error) {
+	s.cache.mu.RLock()
+
+	if s.cache.result != nil && time.Since(s.cache.fetchedAt) < 2*time.Second {
+		s.cache.mu.RUnlock()
+		return s.cache.result, nil
+	}
+
+	s.cache.mu.RUnlock()
+
+	res, e, _ := s.group.Do("currently-playing", func() (any, error) {
+		j := &job{
+			ctx:    ctx,
+			result: make(chan *spotify.CurrentlyPlaying, 1),
+			err:    make(chan error, 1),
+		}
+
+		s.pool.Invoke(j)
+
+		select {
+		case r := <-j.result:
+			s.cache.mu.Lock()
+			s.cache.result = r
+			s.cache.fetchedAt = time.Now()
+			s.cache.mu.Unlock()
+			return r, nil
+		case e := <-j.err:
+			return nil, e
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		}
+	})
+
+	if e != nil {
+		return nil, e
+	}
+
+	return res.(*spotify.CurrentlyPlaying), nil
 }
