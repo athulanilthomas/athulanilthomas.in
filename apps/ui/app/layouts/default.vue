21daab979233bc1066bfb114e0190006a3b08b74
Athul Anil Thomas <athulanilthomas@gmail.com>
2026-03-20 05:17:08 +0000

    feat(ui): add nuxt template

---
+++ apps/ui/app/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ui-aurora-container">
+    <BackgroundAurora
+      :color-stops="['#00DC82', '#00DC82', '#020420', '#00DC82']"
+      :amplitude="1"
+      :blend="0.5"
+      :speed="1.0"
+      :intensity="1.0"
+      class="w-full h-full"
+    />
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .ui-aurora-container {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    overflow: hidden;
+  }
+</style>
