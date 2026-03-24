e92a78ecb5c509406f45a985c760a1668e001a89
Athul Anil Thomas <athulanilthomas@gmail.com>
2026-03-24 12:18:06 +0000

    fix(api): only fetch the last 3 updated repos

--- apps/api/internal/service/github.go
+++ apps/api/internal/service/github.go
@@ -21,7 +21,16 @@ func NewGithubService(client *github.Client) (*GithubService, error) {
 }
 
 func (g *GithubService) GetLastUpdatedRepositories(ctx context.Context) ([]*github.Repository, error) {
-	opts := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
+	// Fetch only the last 3 repos
+	opts := &github.RepositoryListOptions{
+		Type:      "owner",
+		Sort:      "updated",
+		Direction: "desc",
+		ListOptions: github.ListOptions{
+			Page:    1,
+			PerPage: 3,
+		},
+	}
 
 	repos, _, err := g.client.Repositories.List(ctx, g.username, opts)
 
